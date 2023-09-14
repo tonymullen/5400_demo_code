472a115e3ba69f27820037ece681c1dc240acdc1
Tony Mullen <a.mullen@northeastern.edu>
2023-09-13 19:04:06 -0700

    Week 2

--- Week02/05-unification.pl
+++ Week02/05-unification.pl
@@ -1,20 +1,32 @@
 three_of_a_kind(X, X, X).
 
-% test_unify1(X, Y, Z) :-
-%   three_of_a_kind(my_strct(marge, X), my_strct(Y, homer), Z).
+test_unify1(X, Y, Z) :-
+  three_of_a_kind(my_strct(marge, X), my_strct(Y, homer), Z).
 
-% test_unify2(X, Y, Z) :-
-%   three_of_a_kind(my_strct(X, homer), my_strct(Y, homer), Z).
+test_unify2(X, Y, Z) :-
+  three_of_a_kind(my_strct(X, homer), my_strct(Y, homer), Z).
 
-% test_unify3(X, Y, Z) :-
-%   three_of_a_kind(my_strct(X, Y), my_strct(Y, homer), Z).
+test_unify3(X, Y, Z) :-
+  three_of_a_kind(my_strct(X, Y), my_strct(Y, homer), Z).
 
-% test_unify4(X, Y, Z) :-
-%   three_of_a_kind(my_strct(marge, homer), my_strct(X, Y), my_strct(Y, Z)).
+test_unify4(X, Y, Z) :-
+  three_of_a_kind(my_strct(marge, homer), my_strct(X, Y), my_strct(Y, Z)).
 
-% test_unify5(X, Y, Z) :-
-%   three_of_a_kind(my_strct(marge, X), my_strct(Y, Z), Z).
+test_unify5(X, Y, Z) :-
+  three_of_a_kind(my_strct(marge, X), my_strct(Y, Z), Z).
 
-% test_unify6(X, Y, Z) :-
-%   set_prolog_flag(occurs_check, true),
-%   three_of_a_kind(my_strct(marge, X), my_strct(Y, Z), Z).
+test_unify6(X, Y, Z) :-
+  set_prolog_flag(occurs_check, true),
+  three_of_a_kind(my_strct(marge, X), my_strct(Y, Z), Z).
+
+test_unify7(X, Y, Z, A) :-
+  three_of_a_kind(marge-homer-bart, X-Y-Z, A).
+
+test_unify8(X, Y, Z, A) :-
+  three_of_a_kind(marge+homer-bart, X-Y-Z, A).
+
+test_unify9(X, Y, Z, A) :-
+  three_of_a_kind(marge+homer-bart, X+Y-Z, A).
+
+test_unify10(X, Y, Z) :-
+  three_of_a_kind(marge+homer-bart, X-Y, Z).
